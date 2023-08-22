74fcc499cfd277cc5ada6138098b18bcda056d58
Sushant Lamsal <sushantlamsal31@gmail.com>
2023-08-22 13:56:17 +0545

    completed SuperQuality pafe

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -3,7 +3,7 @@ import {products } from '../constants'
 
 const PopularProducts = () => {
   return (
-    <section id="product" className='max-container max-sm:mt-12'>
+    <section id="products" className='max-container max-sm:mt-12'>
       <div className="flex flex-col justify start gap-5">
         <h2 className='text-4xl font-palanquin font-bold'>Our  <span className='text-coral-red'>Popular</span> Products</h2>
         <p className='lg:max-w-lg mt-2 font-montserrat text-slate-grey'>Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value</p>
